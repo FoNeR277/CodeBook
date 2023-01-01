029bc1d74904110ca3ec6d3e53de5e0d5232f2dd
fonerka <bogosny@i.ua>
2023-01-01 17:03:28 +0100

    End of Createing CRUD Section (fron-end & back-end)

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import { Activity } from '../../../app/models/activity';
-import { Button, Item, ItemContent, ItemDescription, ItemGroup, Label, Segment } from 'semantic-ui-react';
+import React, { SyntheticEvent, useState } from 'react';
+import {Button, Item, Label, Segment} from "semantic-ui-react";
+import {Activity} from "../../../app/models/activity";
 
 interface Props {
     activities: Activity[];
     selectActivity: (id: string) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
-export default function ActivityList({ activities, selectActivity, deleteActivity }: Props) {
+export default function ActivityList({activities, selectActivity, deleteActivity, submitting}: Props) {
+    const [target, setTarget] = useState('');
+
+    function handleDeleteActivity(e: SyntheticEvent<HTMLButtonElement>, id: string) {
+        setTarget(e.currentTarget.name);
+        deleteActivity(id)
+    }
+    
     return (
         <Segment>
             <Item.Group divided>
@@ -16,15 +24,19 @@ export default function ActivityList({ activities, selectActivity, deleteActivit
                     <Item key={activity.id}>
                         <Item.Content>
                             <Item.Header as='a'>{activity.title}</Item.Header>
-                            <Item.Meta>{activity.date.toString()}</Item.Meta>
+                            <Item.Meta>{activity.date}</Item.Meta>
                             <Item.Description>
                                 <div>{activity.description}</div>
                                 <div>{activity.city}, {activity.venue}</div>
                             </Item.Description>
                             <Item.Extra>
-                                <Button onClick={() => selectActivity(activity.id)} floated='right' content='View' color='orange' />
-                                <Button onClick={() => deleteActivity(activity.id)} floated='right' content='Delete' color='red' />
-                                <Label basic content={activity.category} />
+                                <Button floated='right' content='View' color='blue'
+                                        onClick={() => selectActivity(activity.id)}/>
+                                <Button loading={submitting && target === activity.id}
+                                        name={activity.id} floated='right' content='Delete'
+                                        color='red'
+                                        onClick={(e) => handleDeleteActivity(e, activity.id)}/>
+                                <Label basic content={activity.category}/>
                             </Item.Extra>
                         </Item.Content>
                     </Item>
@@ -33,3 +45,4 @@ export default function ActivityList({ activities, selectActivity, deleteActivit
         </Segment>
     )
 }
+
