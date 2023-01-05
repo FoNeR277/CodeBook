79798e6b83e9800189f19e333e025c0c8fcfb924
fonerka <bogosny@i.ua>
2023-01-05 21:58:57 +0100

    Add Formik and validation

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -2,6 +2,7 @@ import {makeAutoObservable, runInAction} from "mobx";
 import {Activity} from "../models/activity";
 import agent from "../api/agent";
 import {v4 as uuid} from "uuid";
+import {format} from "date-fns";
 
 export default class ActivityStore {
     activityRegistry = new Map<string, Activity>();
@@ -15,16 +16,14 @@ export default class ActivityStore {
 
     get activitiesByDate(){
         return Array.from(this.activityRegistry.values()).sort((a, b) =>
-        Date.parse(a.date) - Date.parse(b.date));
+        a.date!.getTime() - b.date!.getTime());
     }
 
     get groupedActivities(): [string, Activity[]][] {
-        const sortedActivities = Array.from(this.activityRegistry.values()).sort((a, b) =>
-            Date.parse(a.date) - Date.parse(b.date));
 
         return Object.entries(
-            sortedActivities.reduce((activities, activity) => {
-                const date = activity.date.split('T')[0];
+            this.activitiesByDate.reduce((activities, activity) => {
+                const date = format(activity.date!, 'dd MMM yyyy');
                 activities[date] = activities[date] ? [...activities[date], activity] : [activity];
                 return activities;
             }, {} as {[key: string]: Activity[]})
@@ -72,7 +71,7 @@ export default class ActivityStore {
     }
 
     private setActivity = (activity: Activity) => {
-        activity.date = activity.date.split('T')[0];
+        activity.date = new Date(activity.date!);
         this.activityRegistry.set(activity.id, activity);
     }
 
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -4,6 +4,7 @@ import './app/layout/style.css';
 import 'semantic-ui-css/semantic.min.css';
 import 'react-calendar/dist/Calendar.css';
 import 'react-toastify/dist/ReactToastify.min.css'
+import 'react-datepicker/dist/react-datepicker.css'
 import reportWebVitals from './reportWebVitals';
 import {store, StoreContext} from "./app/stores/store";
 import {RouterProvider} from "react-router-dom";
